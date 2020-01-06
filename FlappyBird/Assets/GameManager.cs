2cd6aa71ed41677ef265e75607472a4522446d48
Prateek Saini <pratiiksaini@gmail.com>
2020-01-06 16:15:35 +0530

    Flappy bird version 1.0

---
+++ FlappyBird/Assets/GameManager.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class GameManager : MonoBehaviour
+{
+    public GameObject score;
+    public GameObject gameOverCanvas;
+    // Start is called before the first frame update
+    private void Start()
+    {
+        Time.timeScale = 1;
+        gameOverCanvas.SetActive(false);
+    }
+
+    public void Gameover()
+    {
+        gameOverCanvas.SetActive(true);
+        score.SetActive(false);
+        Time.timeScale = 0;
+    }
+    // Update is called once per frame
+
+    void Update()
+    {
+        
+    }
+
+    public void Replay()
+    {
+        SceneManager.LoadScene(0);
+        Score.score = 0;
+    }
+}
---
+++ FlappyBird/Assets/Scripts/Score.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Score : MonoBehaviour
+{
+    public static int score;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+        GetComponent<UnityEngine.UI.Text>().text = score.ToString(); 
+    }
+}
